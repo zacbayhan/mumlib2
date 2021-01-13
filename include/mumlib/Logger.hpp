a9ee05dbf161ea98bc955b6b2b8f1ac94809b69d
Mikhail Paulyshka <me@mixaill.tk>
2021-01-14 02:31:09 +0300

    silent logs

--- include/mumlib/Logger.hpp
+++ include/mumlib/Logger.hpp
@@ -14,37 +14,37 @@ namespace mumlib {
 		template<typename... Args>
 		void crit(const std::string& text, Args... args)
 		{
-			std::cout << "[crit]" << text << std::endl;
+		//	std::cout << "[crit]" << text << std::endl;
 		}
 
 		template<typename... Args>
 		void debug(const std::string& text, Args... args)
 		{
-			std::cout << "[debug]" << text << std::endl;
+		//	std::cout << "[debug]" << text << std::endl;
 		}
 
 		template<typename... Args>
 		void error(const std::string& text, Args... args)
 		{
-			std::cout << "[error]" << text << std::endl;
+		//	std::cout << "[error]" << text << std::endl;
 		}
 
 		template<typename... Args>
 		void info(const std::string& text, Args... args)
 		{
-			std::cout << "[info]" << text << std::endl;
+		//	std::cout << "[info]" << text << std::endl;
 		}
 
 		template<typename... Args>
 		void notice(const std::string& text, Args... args)
 		{
-			std::cout << "[notice]" << text << std::endl;
+		//	std::cout << "[notice]" << text << std::endl;
 		}
 
 		template<typename... Args>
 		void warn(const std::string& text, Args... args)
 		{
-			std::cout << "[warn]" << text << std::endl;
+		//	std::cout << "[warn]" << text << std::endl;
 		}
 	};
 }
